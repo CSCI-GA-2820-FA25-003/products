ca06da5f9fd68dd2c279bf068a913abf0a282915
Joannepistulli <41384505+Joannepistulli@users.noreply.github.com>
2025-10-08 15:46:31 -0400

    Add files via upload
    created test cases

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,6 +22,7 @@ TestProducts API Service Test Suite
 import os
 import logging
 from unittest import TestCase
+from tests.factories import ProductsFactory
 from wsgi import app
 from service.common import status
 from service.models import db, Products
@@ -30,6 +31,7 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
 
+BASE_URL = "/products"
 
 ######################################################################
 #  T E S T   C A S E S
@@ -73,3 +75,42 @@ class TestYourResourceService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Todo: Add your test cases here...
+
+    def test_create_product(self):
+            """It should Create a new Product"""
+            test_product = ProductsFactory()
+            logging.debug("Test Product: %s", test_product.serialize())
+            response = self.client.post(BASE_URL, json=test_product.serialize())
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+            # Make sure location header is set
+            location = response.headers.get("Location", None)
+            self.assertIsNotNone(location)
+
+            # Check the data is correct
+            new_product = response.get_json()
+            # self.assertEqual(new_product["id"], test_product.id)
+            self.assertEqual(new_product["name"], test_product.name)
+            self.assertEqual(new_product["description"], test_product.description)
+            self.assertEqual(new_product["price"], test_product.price)
+            self.assertEqual(new_product["image_url"], test_product.image_url)
+            self.assertEqual(new_product["category"], test_product.category)
+            self.assertEqual(new_product["availability"], test_product.availability)
+            self.assertEqual(new_product["created_date"], test_product.created_date)
+            self.assertEqual(new_product["updated_date"], test_product.updated_date)
+            
+            # Todo: Uncomment this code when get_products is implemented 
+
+            # Check that the location header was correct
+            # response = self.client.get(location)
+            # self.assertEqual(response.status_code, status.HTTP_200_OK)
+            # new_product = response.get_json()
+            # self.assertEqual(new_product["id"], test_product.id)
+            # self.assertEqual(new_product["name"], test_product.name)
+            # self.assertEqual(new_product["description"], test_product.description)
+            # self.assertEqual(new_product["price"], test_product.price)
+            # self.assertEqual(new_product["image_url"], test_product.image_url)
+            # self.assertEqual(new_product["category"], test_product.category)
+            # self.assertEqual(new_product["availability"], test_product.availability)
+            # self.assertEqual(new_product["created_date"], test_product.created_date)
+            # self.assertEqual(new_product["updated_date"], test_product.updated_date)
